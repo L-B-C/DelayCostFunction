c625cc4ab56abec805e60afab1adc5c26a0b9590
aroundmidnight <luigibjorn.cortellino@gmail.com>
2024-03-12 13:11:08 +0100

    moved airline folder to CostPackage and added comments

--- CostPackage/Airline/airline.py
+++ CostPackage/Airline/airline.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 
+# airline_static.csv from Mercury Dataset
 df_airlines = pd.read_csv(os.path.join(os.path.dirname(__file__), "airline_static.csv"))
 
 
@@ -20,6 +21,11 @@ def is_valid_airline_icao(airline_icao: str):
         raise AirlineCodeError
 
 
+# AO_type
+# REG - Regional
+# FSC - Full Service Carrier
+# LCC - Low-Cost Carrier
+# CHT - Charter
 def is_LCC_airline_icao(airline_icao: str):
     if is_valid_airline_icao(airline_icao) and df_airlines[df_airlines.ICAO == airline_icao].iloc[0].AO_type == 'LCC':
         return True
